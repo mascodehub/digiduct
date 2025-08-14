00ee2959712843c42254b7246116656341f033ae
wabot <walidshr8@gmail.com>
2025-08-14 23:26:24 +0700

    test:test

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,32 @@
-<script setup></script>
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+      total: 0,
+      a: 0,
+      b: 0,
+    };
+  },
+
+  methods: {
+    increment() {
+      this.count++;
+    },
+    perkalian(x, y) {
+      this.a = x;
+      this.b = y;
+      this.total = this.a * this.b;
+    },
+  },
+
+  mounted() {
+    console.log(`The initial count is ${this.count}.`);
+  },
+};
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <button @click="increment">Count is: {{ count }}</button>
+  <button @click="perkalian(5, 4)">Total is: {{ total }}</button>
 </template>
-
-<style scoped></style>
